0bb2aeca2e007a4155092e5eb99c9b003cb4c739
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-09-03 05:31:37 -0700

    Creating and Adjusting the page

---
+++ src/components/provider/ReplyProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export const ReplyContext = createContext({
+  reply: {
+    id: "",
+    content: "",
+    createdAt: "",
+    score: "",
+    replyingTo: "",
+    user: {
+      image: {
+        png: "",
+        webp: "",
+      },
+      username: "",
+    },
+  },
+});
+
+export const useReplyContext = () => (
+    useContext(ReplyContext)
+)
